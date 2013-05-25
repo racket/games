3258130bc6962976dcd717c22742540a58ecc661
Matthew Flatt <mflatt@racket-lang.org>
2013-05-24 21:57:31 -0600

    reorganize into core plus packages
    The "racket" directory contains a pared-back version of the
    repository, roughly.
    
    The "pkgs" directory everything else in the repository, but
    organized into packages.
    
    original commit: b2ebb0a28bf8136e75cd98316c22fe54c30eacb2

--- pkgs/games/info.rkt
+++ pkgs/games/info.rkt
@@ -1,5 +1,7 @@
 #lang setup/infotab
 
+(define single-collection "games")
+
 (define scribblings '(("scribblings/games.scrbl" (multi-page) (gui-library))))
 
 (define gracket-launcher-libraries (list "main.rkt"))
